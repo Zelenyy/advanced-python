44ed03283ab6fdb72dd58f4e88bc34adb9805412
Mikhal Zelenyy <mihail.zelenyy@phystech.edu>
2022-11-30 20:41:15 +0300

    Lecture 13

---
+++ materials/plot/pause_example.py
@@ -0,0 +1,16 @@
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+# load some test data for demonstration and plot a wireframe
+X, Y, Z = axes3d.get_test_data(0.1)
+ax.plot_wireframe(X, Y, Z, rstride=5, cstride=5)
+
+if __name__ == '__main__':
+    # rotate the axes and update
+    for angle in range(0, 360):
+        ax.view_init(30, angle)
+        plt.draw()
+        plt.pause(.001)
