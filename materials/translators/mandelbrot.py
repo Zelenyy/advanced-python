cb30dc92ed45d81929b1d78b8ab1f93490a4fbfa
Mikhail Zelenyy <mihail.zelenyy@phystech.edu>
2023-03-31 14:49:12 +0300

    From lecture

--- materials/translators/mandelbrot.py
+++ materials/translators/mandelbrot.py
@@ -1,21 +1,20 @@
 import time
 
-import numpy as np
 
 def mandelbrot():
-    pmin, pmax, qmin, qmax = -2.5, 1.5, -2, 2
+    pmin, pmax, qmin, qmax = -250, 150, -200, 200
     ppoints, qpoints = 200, 200
     max_iterations = 400
     infinity_border = 100
-    image = np.zeros((ppoints, qpoints))
-    for ip, p in enumerate(np.linspace(pmin, pmax, ppoints)):
-        for iq, q in enumerate(np.linspace(qmin, qmax, qpoints)):
+    image = [[0 for i in range(qmax - qmin)] for j in range(pmax-pmin)]
+    for ip, p in enumerate(range(pmin, pmax)):
+        for iq, q in enumerate(range(qmin, qmax)):
             c = p + 1j * q
             z = 0
             for k in range(max_iterations):
                 z = z ** 2 + c
                 if abs(z) > infinity_border:
-                    image[ip, iq] = 1
+                    image[ip][iq] = 1
                     break
     return image
 
