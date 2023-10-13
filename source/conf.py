e31b1ff37e2a4b24ecf3029be8b2dbafba66fb8d
Mikhail Zelenyy <mihail.zelenyy@phystech.edu>
2023-10-13 19:36:38 +0300

    From lecture

---
+++ source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Advanced Python'
+copyright = '2023, MIPT'
+author = 'Mikhail Zelenyi'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'jupyterlite_sphinx',
+    'myst_parser',
+    "sphinx_revealjs",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ source/index.md
@@ -0,0 +1,9 @@
+
+# Welcome to Advanced Python's course!
+
+```{include} _slides/1.md
+```
+```{include} _slides/2.md
+```
+```{include} _slides/3.md
+```
